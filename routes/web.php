b3471ab76b6553aceebdc7293949572477723de3
meme <h-kameyama081@jutjoy.com>
2024-02-05 00:54:46 +0900

    4-13

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Admin\SelfProfileController;
+use App\Http\Controllers\Admin\NewsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +19,27 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::controller(NewsController::class)->prefix('admin')->middleware('auth')->name('news.')->group(function () {
+    Route::get('news/create', 'add')->name('add');
+});
+
+Route::controller(SelfProfileController::class)->prefix('admin')->middleware('auth')->name('Profile.')->group(function () {
+    Route::get('profile/create', 'add')->name('add');
+});
+
+Route::controller(SelfProfileController::class)->prefix('admin')->middleware('auth')->name('Profile.')->group(function () {
+    Route::get('profile/edit', 'edit')->name('edit');
+});
+
+require __DIR__ . '/auth.php';
